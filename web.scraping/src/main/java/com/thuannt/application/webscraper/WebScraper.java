2b36bf307dc96bd1852ca85343f1b8e1739251b9
thuannt-se <thuannt.se@gmail.com>
2020-10-27 00:30:35 +0700

    Initial web scraper
    using Template Method design pattern

--- web.scraping/src/main/java/com/thuannt/application/webscraper/WebScraper.java
+++ web.scraping/src/main/java/com/thuannt/application/webscraper/WebScraper.java
@@ -1,5 +1,30 @@
 package com.thuannt.application.webscraper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+
+import com.thuannt.application.model.RealEstate;
+
 public abstract class WebScraper {
 	
+	abstract Iterable<Document> inspectHomePage();
+	
+	abstract Iterable<Document> inspectSubPage(Document subpage);
+	
+	abstract RealEstate inspectDetailPage(Document detailPage);
+	
+	protected Iterable<RealEstate> inspect() {
+		List<RealEstate> realEstates = new ArrayList<>();
+        Iterable<Document> subpages = inspectHomePage();
+        for (Document subpage: subpages) {
+            Iterable<Document> detailPages = inspectSubPage(subpage);
+            for (Document detailPage: detailPages) {
+            	RealEstate classifiedAd = inspectDetailPage(detailPage);
+            	realEstates.add(classifiedAd);
+            }
+        }
+        return realEstates;
+	}
 }
